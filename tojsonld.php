85505a16884199e890e87adf08ff056d3a0aad6a
Roderic Page <rdmpage@gmail.com>
2019-08-03 22:09:13 +1200

    Many changes

--- tojsonld.php
+++ tojsonld.php
@@ -30,6 +30,84 @@ function get($url, $user_agent='', $content_type = '')
 }
 
 //----------------------------------------------------------------------------------------
+// Cites
+function get_wikidata_cites($qid)
+{
+	$list = array();
+	
+	// use group cat to handle cases whre we have multiple titles for an article
+	$sparql = 'SELECT ?work (GROUP_CONCAT(?title;SEPARATOR="/") AS ?name) 
+{
+   wd:' . $qid . '  wdt:P2860 ?work .
+  ?work wdt:P1476 ?title .
+}
+GROUP BY ?work';
+	
+	
+	$url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql?query=' . urlencode($sparql);
+	$json = get($url, '', 'application/json');
+		
+	if ($json != '')
+	{
+		$obj = json_decode($json);
+		if (isset($obj->results->bindings))
+		{
+			foreach ($obj->results->bindings as $binding)
+			{
+				$item = new stdclass;
+
+				$id = $binding->work->value;
+				$id = preg_replace('/https?:\/\/www.wikidata.org\/entity\//', '', $id);
+				
+				$item->id = $id;
+				
+				$item->name = $binding->name->value;
+				
+				$list[] = $item;
+			}
+		}
+	}
+	
+	return $list;
+}
+
+//----------------------------------------------------------------------------------------
+// Cited by
+function get_wikidata_cited_by($qid)
+{
+	$list = array();
+	
+	$sparql = 'SELECT * WHERE {?work wdt:P2860 wd:' . $qid . ' . ?work wdt:P1476 ?title . }';
+	
+	$url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql?query=' . urlencode($sparql);
+	$json = get($url, '', 'application/json');
+		
+	if ($json != '')
+	{
+		$obj = json_decode($json);
+		if (isset($obj->results->bindings))
+		{
+			foreach ($obj->results->bindings as $binding)
+			{
+				$item = new stdclass;
+
+				$id = $binding->work->value;
+				$id = preg_replace('/https?:\/\/www.wikidata.org\/entity\//', '', $id);
+				
+				$item->id = $id;
+				
+				$item->title = $binding->title->value;
+				
+				$list[] = $item;
+			}
+		}
+	}
+	
+	return $list;
+}
+
+
+//----------------------------------------------------------------------------------------
 // get Wikidata record for person
 function get_wikidata_author_ld($qid)
 {
@@ -40,7 +118,7 @@ function get_wikidata_author_ld($qid)
 	$obj = json_decode($json);
 	
 	$author = new stdclass;
-	$author->id = 'https://www.wikidata.org/wiki/' . $qid;
+	$author->id = 'http://www.wikidata.org/entity/' . $qid;
 	
 	$author->names = array();
 	
@@ -80,7 +158,7 @@ function get_wikidata_container_ld(&$triples, $qid)
 		'P236' => '<http://schema.org/issn>',
 	);
 
-	$subject = '<https://www.wikidata.org/wiki/' . $qid . '>';
+	$subject = '<http://www.wikidata.org/entity/' . $qid . '>';
 
 	$url = 'https://www.wikidata.org/wiki/Special:EntityData/' . $qid . '.json';
 
@@ -144,7 +222,18 @@ function get_wikidata_container_ld(&$triples, $qid)
 //----------------------------------------------------------------------------------------
 function wikidata_identifier(&$triples, $subject, $namespace, $value)
 {
-	$bnode = '_:' . $namespace;
+	if (0)
+	{
+		$bnode = '_:' . $namespace;
+	}
+	else
+	{	
+		$subject_id = $subject;
+		$subject_id = str_replace('<', '', $subject_id);
+		$subject_id = str_replace('>', '', $subject_id);
+		
+		$bnode = '<' . $subject_id . '#'  . $namespace . '>';
+	}
 
 	$triple = array();
 	$triple[] = $subject;
@@ -170,8 +259,18 @@ function wikidata_identifier(&$triples, $subject, $namespace, $value)
 	
 	$triple = array();
 	$triple[] = $bnode;
-	$triple[] = '<http://schema.org/value>';					
-	$triple[] = '"' . strtolower($value) . '"';
+	$triple[] = '<http://schema.org/value>';
+	
+	switch ($namespace)
+	{
+		case 'zoobank':
+			$triple[] = '"urn:lsid:zoobank.org:pub:' . strtoupper($value) . '"';
+			break;
+	
+		default:
+			$triple[] = '"' . strtolower($value) . '"';
+			break;
+	}		
 	
 	$triples[] = $triple;
 	
@@ -194,6 +293,15 @@ function wikidata_identifier(&$triples, $subject, $namespace, $value)
 			
 			$triples[] = $triple;
 			break;					
+
+		case 'zoobank':
+			$triple = array();
+			$triple[] = $subject;
+			$triple[] = '<http://schema.org/sameAs>';					
+			$triple[] = '"urn:lsid:zoobank.org:pub:' . strtoupper($value) . '"';
+			
+			$triples[] = $triple;
+			break;					
 			
 		default:
 			break;
@@ -202,7 +310,7 @@ function wikidata_identifier(&$triples, $subject, $namespace, $value)
 
 //----------------------------------------------------------------------------------------
 // get Wikidata record
-function get_wikidata_work_ld(&$triples, $qid)
+function get_wikidata_work_ld(&$triples, $qid, $use_role = true)
 {
 	$property_map = array(
 		'P1476' => '<http://schema.org/name>',
@@ -215,7 +323,9 @@ function get_wikidata_work_ld(&$triples, $qid)
 	
 	);
 
-	$subject = '<https://www.wikidata.org/wiki/' . $qid . '>';
+	$subject_id = 'http://www.wikidata.org/entity/' . $qid;
+
+	$subject = '<' . $subject_id . '>';
 
 
 	$url = 'https://www.wikidata.org/wiki/Special:EntityData/' . $qid . '.json';
@@ -265,7 +375,7 @@ function get_wikidata_work_ld(&$triples, $qid)
 					$triple = array();
 					$triple[] = $subject;
 					$triple[] = '<http://schema.org/isPartOf>';					
-					$triple[] = '<https://www.wikidata.org/wiki/' . $claim->mainsnak->datavalue->value->id . '>';	
+					$triple[] = '<http://www.wikidata.org/entity/' . $claim->mainsnak->datavalue->value->id . '>';	
 					
 					$triples[] = $triple;				
 				
@@ -320,9 +430,6 @@ function get_wikidata_work_ld(&$triples, $qid)
 				}
 				break;			
 			
-			
-				
-
 			// simple properties			
 			case 'P304':
 			case 'P433':
@@ -359,6 +466,13 @@ function get_wikidata_work_ld(&$triples, $qid)
 					wikidata_identifier($triples, $subject, 'jstor', $claim->mainsnak->datavalue->value);					
 				}			
 				break;				
+
+			case 'P2007':
+				foreach ($claims as $claim)
+				{
+					wikidata_identifier($triples, $subject, 'zoobank', $claim->mainsnak->datavalue->value);					
+				}			
+				break;				
 		
 			default:
 				break;
@@ -369,18 +483,15 @@ function get_wikidata_work_ld(&$triples, $qid)
 	ksort($authors);
 	//print_r($authors);
 	
+	$index = 0;
+	
 	foreach ($authors as $k => $v)
 	{
+		$index++;
+		
 		if (is_object($v))
 		{
 			$id = '<' . $v->id . '>';
-
-			$triple = array();
-			$triple[] = $subject;
-			$triple[] = '<http://schema.org/creator>';					
-			$triple[] = $id;
-	
-			$triples[] = $triple;
 	
 			$triple = array();
 			$triple[] = $id;
@@ -409,17 +520,60 @@ function get_wikidata_work_ld(&$triples, $qid)
 				$triples[] = $triple;
 				
 			}
+			
+			if ($use_role)
+			{
+				// Role to hold author position
+				$role_id = '<' . $subject_id . '#role/' . $index . '>';
+				
+				$triple = array();
+				$triple[] = $role_id;
+				$triple[] = '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>';
+				$triple[] = '<http://schema.org/Role>';
+				$triples[] = $triple;
+
+				$triple = array();
+				$triple[] = $role_id;
+				$triple[] = '<http://schema.org/roleName>';
+				$triple[] = '"' . $index . '"';
+				$triples[] = $triple;
+
+				$triple = array();
+				$triple[] = $role_id;
+				$triple[] = '<http://schema.org/creator>';
+				$triple[] = $id;
+				$triples[] = $triple;
+
+				$triple = array();
+				$triple[] = $subject;
+				$triple[] = '<http://schema.org/creator>';
+				$triple[] = $role_id;
+				$triples[] = $triple;
+
+				
+			}
+			else
+			{
+				$triple = array();
+				$triple[] = $subject;
+				$triple[] = '<http://schema.org/creator>';					
+				$triple[] = $id;
+	
+				$triples[] = $triple;			
+			}
+			
+			
 		}
 		else
 		{
-			$bnode = '_:creator' . $k;
-
-			$triple = array();
-			$triple[] = $subject;
-			$triple[] = '<http://schema.org/creator>';					
-			$triple[] = $bnode;
-	
-			$triples[] = $triple;
+			if (0)
+			{
+				$bnode = '_:creator' . $k;
+			}
+			else
+			{
+				$bnode = '<' . $subject_id . '#creator_' . $index . '>';
+			}
 	
 			$triple = array();
 			$triple[] = $bnode;
@@ -434,24 +588,110 @@ function get_wikidata_work_ld(&$triples, $qid)
 			$triple[] = '"' . addcslashes($v, '"') . '"';
 			
 			$triples[] = $triple;
+			
+			if ($use_role)
+			{
+				// Role to hold author position
+				$role_id = '<' . $subject_id . '#role/' . $index . '>';
+				
+				$triple = array();
+				$triple[] = $role_id;
+				$triple[] = '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>';
+				$triple[] = '<http://schema.org/Role>';
+				$triples[] = $triple;
+
+				$triple = array();
+				$triple[] = $role_id;
+				$triple[] = '<http://schema.org/roleName>';
+				$triple[] = '"' . $index . '"';
+				$triples[] = $triple;
+
+				$triple = array();
+				$triple[] = $role_id;
+				$triple[] = '<http://schema.org/creator>';
+				$triple[] = $bnode;
+				$triples[] = $triple;
+
+				$triple = array();
+				$triple[] = $subject;
+				$triple[] = '<http://schema.org/creator>';
+				$triple[] = $role_id;
+				$triples[] = $triple;
+
+				
+			}
+			else
+			{
+				$triple = array();
+				$triple[] = $subject;
+				$triple[] = '<http://schema.org/creator>';					
+				$triple[] = $bnode;
+	
+				$triples[] = $triple;			
+			}			
 		}
+	}
+	
+	
+	// literature cited
+	$list = get_wikidata_cites($qid);
+		
+	$n = count($list);
+	for ($i = 0; $i < $n; $i++)
+	{
+		$cited_id = '<http://www.wikidata.org/entity/' . $list[$i]->id . '>';
+	
+	
+		$triple = array();
+		$triple[] = $subject;
+		$triple[] = '<http://schema.org/citation>';					
+		$triple[] = $cited_id ;
+		
+		$triples[] = $triple;		
+		
+		// Add a name (which may be a concatentation of multiple languages)
+		// so that we can make the citation more infomrative than just an id
+		$triple = array();
+		$triple[] = $cited_id ;
+		$triple[] = '<http://schema.org/alternateName>';					
+		$triple[] = '"' . addcslashes($list[$i]->name, '"') . '"';
+		
+		$triples[] = $triple;		
+		
 	}
 
 
 }
+
+$use_role = true;
+	
 
 
 $qid = 'Q58837514';
 
 $qid = 'Q42258926';
 
-$qid = 'Q58676985';
+//$qid = 'Q58676985';
 
-$qid = 'Q47164672'; // 毛尾足螨屬3新種(蜱螨亞綱:尾足螨股)
+//$qid = 'Q47164672'; // 毛尾足螨屬3新種(蜱螨亞綱:尾足螨股)
 
-//$qid = 'Q29035814';
+$qid = 'Q29035814';
 
 //$qid = 'Q28944234';
+
+$qid = 'Q49877817';
+
+//$qid = 'Q58838447';
+
+$qid = 'Q58837522';
+
+$qid = 'Q28937258';
+
+$qid = 'Q21191815';
+
+//$qid = 'Q47164672'; // 毛尾足螨屬3新種(蜱螨亞綱:尾足螨股)
+
+$qid = 'Q28240690';
 
 $triples = array();
 
@@ -466,50 +706,67 @@ foreach ($triples as $triple)
 	$nt .= join(' ', $triple) . ' .' . "\n";
 }
 
-//echo $nt;
+if (0)
+{
+	echo $nt;
+	echo "\n";
+}
+else
+{
 
-$doc = jsonld_from_rdf($nt, array('format' => 'application/nquads'));
+	$doc = jsonld_from_rdf($nt, array('format' => 'application/nquads'));
 
-//print_r($doc);
+	// Frame it-------------------------------------------------------------------------------
 
-// Frame it-------------------------------------------------------------------------------
-
-// Identifier is always an array
-$identifier = new stdclass;
-$identifier->{'@id'} = "http://schema.org/identifier";
-$identifier->{'@container'} = "@set";
+	// Identifier is always an array
+	$identifier = new stdclass;
+	$identifier->{'@id'} = "http://schema.org/identifier";
+	$identifier->{'@container'} = "@set";
 	
-// Creator is always an array
-$creator = new stdclass;
-$creator->{'@id'} = "http://schema.org/creator";
-$creator->{'@container'} = "@set";
+	if ($use_role)
+	{
+		$creator = new stdclass;
+		$creator->{'@id'} = "http://schema.org/creator";
+	}
+	else
+	{
+		// Creator is always an array
+		$creator = new stdclass;
+		$creator->{'@id'} = "http://schema.org/creator";
+		$creator->{'@container'} = "@set";
+	}
+	
+	// sameAs is always an array
+	$sameAs = new stdclass;
+	$sameAs->{'@id'} = "http://schema.org/sameAs";
+	$sameAs->{'@container'} = "@set";
 
-// Creator is always an array
-$sameAs = new stdclass;
-$sameAs->{'@id'} = "http://schema.org/sameAs";
-$sameAs->{'@container'} = "@set";
+	// citation is always an array
+	$citation = new stdclass;
+	$citation->{'@id'} = "http://schema.org/citation";
+	$citation->{'@container'} = "@set";
 
-// Context to set vocab to schema
-$context = new stdclass;
-$context->{'@vocab'} = "http://schema.org/";
+	// Context to set vocab to schema
+	$context = new stdclass;
+	$context->{'@vocab'} = "http://schema.org/";
 
-$context->creator = $creator;
-$context->identifier = $identifier;
-$context->sameAs = $sameAs;
+	$context->creator = $creator;
+	$context->identifier = $identifier;
+	$context->sameAs = $sameAs;
 
-$frame = (object)array(
-	'@context' => $context,
+	$frame = (object)array(
+		'@context' => $context,
 
-	// Root on article
-	'@type' => 'http://schema.org/ScholarlyArticle',
+		// Root on article
+		'@type' => 'http://schema.org/ScholarlyArticle',
 
-);	
+	);	
 
-$framed = jsonld_frame($doc, $frame);
+	$framed = jsonld_frame($doc, $frame);
 
-// Note JSON_UNESCAPED_UNICODE so that, for example, Chinese characters are not escaped
-echo json_encode($framed, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-echo "\n";
-
+	// Note JSON_UNESCAPED_UNICODE so that, for example, Chinese characters are not escaped
+	echo json_encode($framed, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+	echo "\n";
+}
 
 ?>
